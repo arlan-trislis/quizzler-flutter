f8fcd5041d3bafdb01e71c9aa2316230e4af801d
arlan-trislis <arlanariandi@gmail.com>
2020-05-29 10:20:07 +0700

    return main dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:quizzler/quiz_brain.dart';
+
+QuizBrain quizBrain = QuizBrain();
 
 void main() => runApp(Quizzler());
 
@@ -25,6 +28,10 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Widget> scoreKeeper = [];
+
+  int quistionsNumber = 0;
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -37,7 +44,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                quizBrain.questionBank[quistionsNumber].questionText,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -62,6 +69,18 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked true.
+                bool corectAnswer =
+                    quizBrain.questionBank[quistionsNumber].questionAnswer;
+
+                if (corectAnswer == true) {
+                  print("user got it right!");
+                } else {
+                  print("user got it wrong");
+                }
+                setState(() {
+                  quistionsNumber++;
+                });
+                print(quistionsNumber);
               },
             ),
           ),
@@ -80,6 +99,18 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked false.
+                bool corectAnswer =
+                    quizBrain.questionBank[quistionsNumber].questionAnswer;
+
+                if (corectAnswer == false) {
+                  print("user got it right!");
+                } else {
+                  print("user got it wrong");
+                }
+                setState(() {
+                  quistionsNumber++;
+                });
+                print(quistionsNumber);
               },
             ),
           ),
